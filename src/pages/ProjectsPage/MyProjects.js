15da0fc5109ce3f6c0bf3cafac4d9ff1f37ef864
Josh Shearer <ShearerJosh@hotmail.com>
2022-05-12 12:26:39 -0700

    skills update

--- src/pages/ProjectsPage/MyProjects.js
+++ src/pages/ProjectsPage/MyProjects.js
@@ -17,15 +17,15 @@ export const MyProjects = [
   },
   {
     id: 1,
-    title: "Web3 Blockchain Marketplace",
-    skills: "Scrum, Design Patters, Python OOP, API interaction, Authorization",
-    toolset: "Javscript, React, Solidity ",
-    notableLibraries: "Material UI",
-    projectOverview: "",
+    title: "Web3 MTB Token Exchange",
+    skills: "Web 3, Heroku, Blockchain, Redux/Rematch",
+    toolset: "Javscript, React, Nextjs, Typescript, Solidity, Truffle, Ganache",
+    notableLibraries: "Nextjs, Typescript, Redux/Rematch, Tailwindui, Headwindui, Solidity",
+    projectOverview: "This is built with a Nextjs/Typescript front-end.  It will pull in HD Account data via the metamask add-in in the browser.  The exchange will execute Deposit/Withdraw and buy/sell orders for the local MTB for ETH.  Contracts of written in solidity.",
     keyLessons:
-      "This project is ongoing and has been a great opportunity to test trading strategies and free up some personal time in the process.",
-    demoLink: "http://react-traveldiary.web.app",
-    githubLink: "https://github.com/JoshShearer/",
+      "This project is was a great opportunity to learn modern front end build stacks and incorporating code reliability via typescript, in addition to getting great experience in using and writing web 3 contracts in solidity.",
+    demoLink: "https://my-crypto-exchange.herokuapp.com/",
+    githubLink: "https://github.com/JoshShearer/MyCryptoExchange",
     imageLink:
       "https://bitcoinexchangeguide.com/wp-content/uploads/2019/03/Delving-into-Blockchain-as-the-Origin-Point-for-the-new-Generation-of-dApps-on-Web-3-0.jpg",
   },
@@ -39,7 +39,7 @@ export const MyProjects = [
     projectOverview: "This was a great project that ended up automating the brewing process and allowed me to really hone in on the recipe process and make the great beers fully repeatable and reliable.  Those great brews are no longer as the behest of chance occurances and minute differences in the recipe that might vary slightly in the manual process.",
     keyLessons:
       "This project proved very challenging in the breadth required for completion.  Hardware design & procurement, soldiering, fabricating, high and low voltage design as well as knowledge of Python in addition to sensor data verification and aquisition were all required to get this system up and running.",
-    demoLink: "http://react-traveldiary.web.app",
+    // demoLink: "http://react-traveldiary.web.app",
     githubLink: "https://github.com/JoshShearer/AutomatedBrew",
     imageLink: "https://up.picr.de/39892247gq.jpeg",
   },
@@ -53,7 +53,7 @@ export const MyProjects = [
     projectOverview: "This project was an attempt to port several years of nutritional macro data from MyFitnessPal to Cronometer.  There was no tools at the time to do this.  I was able to learn and combine several tools to take data exported to .csv from MyFitnessPal and import the macro data into Cronometer.  This project proved very challenging as the tools are functional but not always reliable and sometimes at the whim of page loading times or internet speed.  In addition to the macro nutrient data, I imported all biometric data from sources like wellnessfx.com.",
     keyLessons:
       "Selenium is a great automation tool, but can be very particular in accessing data and waiting on pages to load.  The design process required the assumption that the script would break and need to restart was random intervals.",
-    demoLink: "http://react-traveldiary.web.app",
+    // demoLink: "http://react-traveldiary.web.app",
     githubLink: "https://github.com/JoshShearer/HealthDataTransfer",
     imageLink:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.v_lPMNggFGsYMVp_8tR8iAHaHo%26pid%3DApi&f=1",
@@ -67,7 +67,7 @@ export const MyProjects = [
     projectOverview: "This project was meant to learn more about my body and how it specifically processes different sources or glucose (sugar).  I used a Dexcom G5 sensor and xDrip to gather and log the raw CGM data.  I them used several Python tools to generate visual aids to viewing understanding how my body reacts to glucose.  These reactions include amplitude and rate of spikes and troughs, how it relates to food eaten and whether exercise occurred slightly before or after the meal.  Several visual aids were used to see how Food, Glucose, Exercise and Sleep all interact to better understand and minimize glucose spikes in my body.",
     keyLessons:
       "This project ended up being much more time consuming that expected.  It allowed me to really learn the power of tools like Pandas and Numpy and the tools used in visualizing.",
-    demoLink: "http://react-traveldiary.web.app",
+    // demoLink: "http://react-traveldiary.web.app",
     githubLink: "https://github.com/JoshShearer/CGMAnalysis",
     imageLink:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.HJ2iPwhHfk6GfM5BWbfadAHaE8%26pid%3DApi&f=1",
--- src/pages/TechnicalPage/SkillSection.js
+++ src/pages/TechnicalPage/SkillSection.js
@@ -17,28 +17,39 @@ const Image = styled.img`
   ${'' /* height: auto; */}
 `;
 const skillImages = [
+  ["React","https://www.mindinventory.com/blog/wp-content/uploads/2017/08/reactjs.jpg"],
+  ["ReactN","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.mm8JucFufPWr2ls_I1h_0wHaDt%26pid%3DApi&f=1"],
+  ["Nextjs","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.IbzKrKUVsfSH_UJxSX53EQHaEK%26pid%3DApi&f=1"],
+  ["Typescript","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SSo_VpLEJ49WTi_ubpTVGQHaDa%26pid%3DApi&f=1"],
+
+  ["CSS","https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/90562701/original/2efb936eb6ad65c1257e56f9ff1c16ab104bbf8d/fix-any-html-css-javascript-errors-website-bugs-or-issues.png"],
+  ["Tailwindui","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.C2pQVFQywHnv6whsimADlgHaEo%26pid%3DApi&f=1"],
+  ["MaterialUI @MUI","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._mdpsmNUZ05vQb-q09t3jAHaDc%26pid%3DApi&f=1"],
+
+  ["MongoDB","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.qQwwv7aYYcjY1q42pOzPFwHaEL%26pid%3DApi&f=1"],
+  ["SQL","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.srRWCNh2uhYtVQ0RcZXfpwHaD7%26pid%3DApi&f=1"],
+
+  ["NodeJS","https://www.hostingadvice.com/wp-content/uploads/2018/09/node-js-logo.jpg"],
+  ["JS","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.NBqrkgXTj3qPB5cZBXPFNQHaD1%26pid%3DApi&f=1"],
+  
+  ["FireB","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Pcwt9CLXrUlZQAUHTH9_SwHaDt%26pid%3DApi&f=1"],
+  ["Heroku","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zOmlX_YQko_-_6JV0Ai_XQHaDI%26pid%3DApi&f=1"],
+  ["Docker",'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E50RgHPgzJ5ZA_5HgUhXDQHaD4%26pid%3DApi&f=1'],
+
+  ["Web3","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.AK6zWN12xUAhM9H4GE7QqAHaEa%26pid%3DApi&f=1"],
+  ["Solidity", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.scrvt.com%2Fb095ee27d37b3d7b6b150adba9ac6ec8%2F5677e4f602626f21%2F0dd4688ca630%2Fv%2F5d972aa4629a%2Fsolidity_overview.png&f=1&nofb=1"],
+  ["Ethereum", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.GuRtgayFyV4btd7YVZqosgHaEK%26pid%3DApi&f=1"],
+  
   ["Python","https://www.technotification.com/wp-content/uploads/2017/05/Python-programming-compressed.jpg"],
+  ["Pandas", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.BULwTfynXVAEDRtDYrB5dQAAAA%26pid%3DApi&f=1"],
+  ["Numpy","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.C3w3zZRyuOfaxzxVBZABWwHaC9%26pid%3DApi&f=1"],
   ["TensorFlow","https://i.ytimg.com/vi/MrijcdNl_U4/maxresdefault.jpg"],
   ["NLP","https://www.freelancinggig.com/blog/wp-content/uploads/2017/07/Natural-Language-Processing.png"],
   ["AlgoTrad","https://www.digitaledge.org/wp-content/uploads/2018/02/How-Can-You-Master-Algorithmic-Trading.jpeg"],
-  ["SQL","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.srRWCNh2uhYtVQ0RcZXfpwHaD7%26pid%3DApi&f=1"],
-  ["React","https://www.mindinventory.com/blog/wp-content/uploads/2017/08/reactjs.jpg"],
-  ["CSS","https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/90562701/original/2efb936eb6ad65c1257e56f9ff1c16ab104bbf8d/fix-any-html-css-javascript-errors-website-bugs-or-issues.png"],
-  ["FireB","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Pcwt9CLXrUlZQAUHTH9_SwHaDt%26pid%3DApi&f=1"],
-  ["ReactN","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.mm8JucFufPWr2ls_I1h_0wHaDt%26pid%3DApi&f=1"],
-  ["NodeJS","https://www.hostingadvice.com/wp-content/uploads/2018/09/node-js-logo.jpg"],
-  ["JS","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.NBqrkgXTj3qPB5cZBXPFNQHaD1%26pid%3DApi&f=1"],
-  ["MongoDB","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.qQwwv7aYYcjY1q42pOzPFwHaEL%26pid%3DApi&f=1"],
-  ["Heroku","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zOmlX_YQko_-_6JV0Ai_XQHaDI%26pid%3DApi&f=1"],
-  ["Docker",'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E50RgHPgzJ5ZA_5HgUhXDQHaD4%26pid%3DApi&f=1'],
-  ["Web3","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.AK6zWN12xUAhM9H4GE7QqAHaEa%26pid%3DApi&f=1"],
   ["Selenium", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Q2nMVNtPBhI0peXXQTSAxQHaEZ%26pid%3DApi&f=1"],
-  ["Pandas", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.BULwTfynXVAEDRtDYrB5dQAAAA%26pid%3DApi&f=1"],
-  ["Numpy","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.C3w3zZRyuOfaxzxVBZABWwHaC9%26pid%3DApi&f=1"],
   ["Craft Beer Pi", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fis4-ssl.mzstatic.com%2Fimage%2Fthumb%2FPurple19%2Fv4%2F99%2Fc1%2F55%2F99c155c4-462e-ecc9-6457-6a0aee30f735%2Fsource%2F512x512bb.jpg&f=1&nofb=1"],
-  ["Solidity", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.scrvt.com%2Fb095ee27d37b3d7b6b150adba9ac6ec8%2F5677e4f602626f21%2F0dd4688ca630%2Fv%2F5d972aa4629a%2Fsolidity_overview.png&f=1&nofb=1"],
-  ["Ethereum", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.GuRtgayFyV4btd7YVZqosgHaEK%26pid%3DApi&f=1"],
 ];
+
 export default function SkillSection(props) {
   return (
     <div>
