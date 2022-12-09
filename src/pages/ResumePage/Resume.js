188d57d90efeaaca22242799bafec3aea101266f
Josh Shearer <joshshear@gmail.com>
2022-12-09 12:03:01 -0600

    resume updates

--- src/pages/ResumePage/Resume.js
+++ src/pages/ResumePage/Resume.js
@@ -3,15 +3,12 @@ import { styled } from "@mui/material/styles";
 import RenderSkills from "./Skills";
 
 // @mui/icons-material
-import {
-  LinkedIn,
-  GitHub,
-  Instagram,
-} from "@mui/icons-material";
+import { LinkedIn, GitHub, Instagram } from "@mui/icons-material";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
-import { Grid, Card, ButtonBase, Typography, IconButton, Tooltip, } from "@mui/material";
+import MKTypography from "components/MKTypography";
+import { Grid, Card, ButtonBase, IconButton, Tooltip } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import HeaderOne from "layouts/sections/navigation/HeaderOne/HeaderOne";
 
@@ -49,6 +46,7 @@ function Resume() {
         }}
       >
         <MKBox component="section" my={6} py={6}>
+          {/* <Container> */}
           <Grid
             container
             spacing={2}
@@ -56,118 +54,135 @@ function Resume() {
             justifyContent="center"
             display="flex"
           >
-            <Grid item sx={{ mx: "auto" }} xs={3}>
-              <ButtonBase sx={{ width: 200, height: 200, left: "25%" }}>
-                <Img src={profile} />
-              </ButtonBase>
-              <br />
-              <br />
-              <Typography fontFamily="cambria" variant="h2" textAlign="center">
-                Josh Shearer
-              </Typography>
-              <Typography
-                fontFamily="cambria"
-                variant="h4"
-                fontWeight="light"
-                textAlign="center"
-              >
-                Lead Full Stack Developer
-              </Typography>
-              <Typography fontFamily="cambria" textAlign="center">
-                <LocationOnIcon />
-                Austin, TX
-              </Typography>
-              <br />
-              <Typography fontFamily="cambria" variant="h4" fontWeight="light">
-                360.461.3810
-              </Typography>
-              <Typography fontFamily="cambria" variant="h4" fontWeight="light">
-                <a href="mailto:ShearerJosh@hotmail.com">
-                  ShearerJosh@hotmail.com
-                </a>
-              </Typography>
-              <Tooltip
-                id="github"
-                title="Check out my Github"
-                placement={window.innerWidth > 959 ? "top" : "left"}
-              >
-                <IconButton
-                  href="https://github.com/JoshShearer"
-                  target="_blank"
-                  size="large"
-                  // color="secondary"
+            {/* <Container> */}
+              <Grid item sx={{ mx: "auto" }} xs={12} lg={3}>
+                <Grid display="flex" flexDirection="row" justifyContent="center">
+                  <ButtonBase sx={{ width: 200, height: 200, }} >
+                    <Img src={profile} />
+                  </ButtonBase>
+                </Grid>
+                <br />
+                <br />
+                <MKTypography
+                  fontFamily="cambria"
+                  variant="h2"
+                  textAlign="center"
+                  
                 >
-                  <GitHub />
-                </IconButton>
-              </Tooltip>
+                  Josh Shearer
+                </MKTypography>
+                <MKTypography
+                  fontFamily="cambria"
+                  variant="h4"
+                  fontWeight="light"
+                  textAlign="center"
+                >
+                  Lead Full Stack Developer
+                </MKTypography>
+                <MKTypography fontFamily="cambria" textAlign="center">
+                  <LocationOnIcon />
+                  Austin, TX
+                </MKTypography>
+                <br />
+                <MKTypography
+                  fontFamily="cambria"
+                  variant="h4"
+                  fontWeight="light"
+                >
+                  360.461.3810
+                </MKTypography>
+                <MKTypography
+                  fontFamily="cambria"
+                  variant="h4"
+                  fontWeight="light"
+                >
+                  <a href="mailto:Josh.Shearer@Zenify.Dev">
+                    Josh.Shearer@Zenify.Dev
+                  </a>
+                </MKTypography>
+                <Tooltip
+                  id="github"
+                  title="Check out my Github"
+                  placement={window.innerWidth > 959 ? "top" : "left"}
+                >
+                  <IconButton
+                    href="https://github.com/JoshShearer"
+                    target="_blank"
+                    size="large"
+                    // color="secondary"
+                  >
+                    <GitHub />
+                  </IconButton>
+                </Tooltip>
 
-              <Tooltip
-                id="linkedin"
-                title="My LinkedIn"
-                size="large"
-                placement={window.innerWidth > 959 ? "top" : "left"}
-              >
-                <IconButton
-                  href="https://www.linkedin.com/in/joshshearer/"
-                  target="_blank"
-                  // color="secondary"
-                >
-                  <LinkedIn />
-                </IconButton>
-              </Tooltip>
-              <Tooltip
-                id="instagram-tooltip"
-                title="Follow me on instagram"
-                placement={window.innerWidth > 959 ? "top" : "left"}
-              >
-                <IconButton
-                  href="https://www.instagram.com/j_shearer83"
-                  target="_blank"
+                <Tooltip
+                  id="linkedin"
+                  title="My LinkedIn"
                   size="large"
-                  // color="secondary"
+                  placement={window.innerWidth > 959 ? "top" : "left"}
                 >
-                  <Instagram />
-                </IconButton>
-              </Tooltip>
-              <br />
-              <br />
-              <Typography fontFamily="cambria" variant="h4">
-                About Me
-              </Typography>
-              <br />
-              <Typography fontFamily="cambria" variant="body1">
-                I enjoy building applications that provide a great user
-                experience and solve difficult problems without compromising
-                reliability or security.
-              </Typography>
-            </Grid>
+                  <IconButton
+                    href="https://www.linkedin.com/in/joshshearer/"
+                    target="_blank"
+                    // color="secondary"
+                  >
+                    <LinkedIn />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip
+                  id="instagram-tooltip"
+                  title="Follow me on instagram"
+                  placement={window.innerWidth > 959 ? "top" : "left"}
+                >
+                  <IconButton
+                    href="https://www.instagram.com/j_shearer83"
+                    target="_blank"
+                    size="large"
+                    // color="secondary"
+                  >
+                    <Instagram />
+                  </IconButton>
+                </Tooltip>
+                <br />
+                <br />
+                <MKTypography fontFamily="cambria" variant="h4">
+                  About Me
+                </MKTypography>
+                <br />
+                <MKTypography fontFamily="cambria" variant="body1">
+                  I enjoy building applications that provide a great user
+                  experience and solve difficult problems without compromising
+                  reliability or security.
+                </MKTypography>
+              </Grid>
+            {/* </Container> */}
             <Grid
               item
-              xs={12}
+              xs={10}
               lg={6}
               flexDirection="column"
               alignItems="left"
-              sx={{ textAlign: "left", my: 6, mx: "auto", px: 0.75 }}
+              sx={{ textAlign: "left", my: 6, mx: "auto", px: 1 }}
             >
-              <Typography
+              <MKTypography
                 fontFamily="cambria"
                 variant="h4"
                 fontWeight="bold"
                 textAlign="center"
               >
                 Work Experience
-              </Typography>
+              </MKTypography>
               <br />
               {ResumeDetails.Experience.jobs.map((job, key) => (
                 <div>
-                  <Typography
+                  <MKTypography
                     fontFamily="cambria"
                     variant="h5"
                     fontWeight="bold"
                   >
                     {job.Company + " - " + job.Title}
-                  </Typography>
-                  <Typography
+                  </MKTypography>
+                  <MKTypography
                     fontFamily="cambria"
                     variant="h6"
                     color="text"
@@ -176,25 +191,25 @@ function Resume() {
                     fontStyle="italic"
                   >
                     {job.StartDate + " - " + job.EndDate}
-                  </Typography>
-                  <Typography
+                  </MKTypography>
+                  <MKTypography
                     fontFamily="cambria"
                     variant="body2"
                     color="black"
                   >
                     {job.Overview}
-                  </Typography>
+                  </MKTypography>
                   <br />
                   <ul>
                     {job.Bullets.map((point) => (
-                      <Typography
+                      <MKTypography
                         fontFamily="cambria"
                         variant="body2"
                         color="black"
                         fontWeight="light"
                       >
                         <li>{point}</li>
-                      </Typography>
+                      </MKTypography>
                     ))}
                   </ul>
                   <br />
@@ -203,30 +218,35 @@ function Resume() {
               <br />
               <RenderSkills Skills={ResumeDetails.Skills.Breakout} />
               <br />
-              <Typography
+              <MKTypography
                 fontFamily="cambria"
                 variant="h4"
                 fontWeight="bold"
                 textAlign="center"
               >
                 Education
-              </Typography>
+              </MKTypography>
               {ResumeDetails.Education.Degrees.map((degree) => (
                 <div>
-                  <Typography
+                  <MKTypography
                     fontFamily="cambria"
                     variant="h6"
                     fontWeight="bold"
                   >
                     {degree[0]}
-                  </Typography>
-                  <Typography fontFamily="cambria" variant="body2" color="text">
+                  </MKTypography>
+                  <MKTypography
+                    fontFamily="cambria"
+                    variant="body2"
+                    color="text"
+                  >
                     {degree[1]}
-                  </Typography>
+                  </MKTypography>
                 </div>
               ))}
             </Grid>
           </Grid>
+          {/* </Container> */}
         </MKBox>
       </Card>
       <MKBox pt={6} px={1} mt={6}>
