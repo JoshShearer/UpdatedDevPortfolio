4a2beb4b9697c691b00010e9dab988dd05711945
Josh Shearer <joshshear@gmail.com>
2024-12-23 22:26:50 -0600

    cleanup

--- src/Comps/routers/Primary/index.tsx
+++ src/Comps/routers/Primary/index.tsx
@@ -27,19 +27,3 @@ export const routes =
     </Route>
   )
   export const Comps_routers_Primary = createBrowserRouter(routes);
-
-// export const Comps_routers_Limited = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="" element={<Comps_routers_Root />}>
-//       <Route path="/" element={<Pages_landing />} />
-//       <Route path="/thankyou" element={<Pages_thankYou />} />
-//       <Route path="/mushrooms/news" element={<Pages_news />} />
-//       <Route path="/mushrooms/news/:title/:id" element={<Pages_article />} />
-//       <Route path="/login" element={<Pages_signIn />} />
-//       <Route path="/signup" element={<Pages_signUp />} />
-//       <Route path="/forgotPw" element={<Pages_forgotPw />} />
-//       <Route path="/thankyou" element={<Pages_thankYou />} />
-//       <Route path="*" element={<Pages_notFound />} />
-//     </Route>
-//   )
-// );
